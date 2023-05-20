4dc8c64ae54ea19c62cd6fae97d8c29feb9ac380
ilias <hliaslago@gmail.com>
2023-05-20 19:11:37 +0300

    js api done

--- api/login.js
+++ api/login.js
@@ -1,1 +1,38 @@
-let loginButton = document.getElementById("submit");
+/*
+Make sure to include <script src="https://unpkg.com/axios/dist/axios.min.js"></script> 
+in order to use axios.js for http requests
+
+The object containing user's credentials will be sent with a POST request on 
+endpoint '/api/v1/auth/authenticate'
+
+*/ 
+
+let loginButton = document.getElementById("submitButton");
+
+let emailField = document.getElementById("email");
+
+let passwordField = document.getElementById("password");
+
+loginButton.addEventListener("click", function(){
+    email = emailField.value;
+    password = passwordField.value;
+    
+
+    if(!email || !password){
+        //console.log("Empty Field");
+    }
+    else{
+        const json = {
+            email: email,
+            password: password
+        };
+        axios.post('/api/v1/auth/authenticate', json)
+            .then(function (response) {
+                console.log(response);
+                window.location.replace("/");
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+});
---
+++ api/register.js
@@ -0,0 +1,44 @@
+/*
+Make sure to include <script src="https://unpkg.com/axios/dist/axios.min.js"></script> 
+in order to use axios.js for http requests
+
+The object containing user's credentials will be sent with a POST request on 
+endpoint '/api/v1/auth/register'
+
+*/ 
+
+let loginButton = document.getElementById("submitButton");
+
+let emailField = document.getElementById("email");
+
+let passwordField = document.getElementById("password");
+
+let firstnameField = document.getElementById("firstname");
+
+let lastnameField = document.getElementById("lastname");
+
+loginButton.addEventListener("click", function(){
+    let email = emailField.value;
+    let password = passwordField.value;
+    let firstname = firstnameField.value;
+    let lastname = lastnameField.value;
+
+    if(email && password && firstname && lastname){
+        
+        const json = {
+            email: email,
+            password: password,
+            firstname: firstname,
+            lastname: lastname
+        };
+        
+        axios.post('/api/v1/auth/register', json)
+            .then(function (response) {
+                console.log(response);
+                window.location.replace("/");
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+});
